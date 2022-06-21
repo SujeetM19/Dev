2be4006f84d1dc9845318d5069fa4d3bead87a2d
SujeetM19 <sujeetmishra008@gmail.com>
2022-06-22 01:04:14 +0530

    .

---
+++ JS/concepts/index.js
@@ -0,0 +1,18 @@
+//variable declaraion
+var a;
+
+a=10;   //variable initialisation
+console.log(a);
+
+console.log(typeof a); //typeof operator
+
+a="sujeet";
+console.log(typeof a);
+
+a = null;
+console.log(a);
+console.log(typeof a);
+
+
+
+
