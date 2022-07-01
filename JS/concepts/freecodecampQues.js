03174c4beb765676832b3b1ba1c412d1e1205632
SujeetM19 <sujeetmishra008@gmail.com>
2022-07-01 14:22:31 +0530

    free code camp ques 1

---
+++ JS/concepts/freecodecampQues.js
@@ -0,0 +1,215 @@
+//ques 1 :- commenting inline and multiline
+
+//hi this is inline comment
+
+/* this is multiline comment
+*/
+
+
+//QUes 2 :- Declare JavaScript Variables
+
+var myName;
+
+//Ques 3:-
+//Storing Values with the Assignment Operator
+
+var a;
+a = 7;
+
+//Ques 4 :- //Assigning the Value of One Variable to Another
+var a;
+a = 7;
+var b;
+b=a;
+
+//Ques 5 :- Initializing Variables with the Assignment Operator
+
+var a = 9;
+
+//Ques 6 :- Declare String Variables
+
+var myFirstName = "Sujeet";
+var myLastName = "Kumar";
+
+//Ques 6:- Understanding Uninitialized Variables
+
+// Only change code below this line
+var a;
+var b;
+var c;
+a = 5;
+b = 10;
+c = "I am a";
+// Only change code above this line
+ 
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+
+//Ques 7 :- Understanding Case Sensitivity in Variables camelCase
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//Ques 8 :- Explore Differences Between the var and let Keywords
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//Ques 9:-Declare a Read-Only Variable with the const Keyword
+
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+
+//Ques 10:- Add Two Numbers with JavaScript
+
+const sum = 10 + 10;
+
+
+//Ques 11:- Subtract One Number from Another with JavaScript
+
+const difference = 45 - 33;
+
+
+//Ques 12:- Multiply Two Numbers with JavaScript
+
+const produc = 8 * 10;
+
+//Ques 13 :- Divide One Number by Another with JavaScript
+
+const quotien = 66 / 33;
+
+
+//Ques 14:- Increment a Number with JavaScript
+
+let myVar = 87;
+
+// Only change code below this line
+myVar = myVar + 1;
+myVar++;
+
+//Ques 14:- Increment a Number with JavaScript
+
+//let myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+
+//QUes 15 ::- Decrement a Number with JavaScript
+//let myVar = 11;
+
+// Only change code below this line
+myVar--;
+
+
+
+//QUes  16:- Create Decimal Numbers with JavaScript
+const ourDecimal = 5.7;
+
+// Only change code below this line
+
+var myDecimal = 5.7;
+
+
+//QUes 17 :- Multiply Two Decimals with JavaScript
+
+const product = 2.0 * 2.5;
+
+
+//Ques 18 :- Divide One Decimal by Another with JavaScript
+
+const quotient = 4.4 / 2.0; // Change this line
+
+
+//Ques 19:- Finding a Remainder in JavaScript
+
+
+const remainder = 11%3;
+
+
+//Ques 20:- Compound Assignment With Augmented Addition
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+
+//Ques11: Compound Assignment With Augmented Subtraction
+
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+
+//Ques21: Compound Assignment With Augmented Subtraction
+
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+
+// Ques 22:- Compound Assignment With Augmented Multiplication
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+
+//Ques 23:- Compound Assignment With Augmented Division
+
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /=12;
+b /=4;
+c /=11;
+
+//Ques 24:- Escaping Literal Quotes in Strings
+
+const myStr = "I am a \"double quoted\" string insile \"double quotes\""; // Change this line
+
+
+
+
+
+
+
+
