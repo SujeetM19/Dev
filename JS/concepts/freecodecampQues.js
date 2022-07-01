93e1806a6eb27d70a8f7499e66563b49fa718548
SujeetM19 <sujeetmishra008@gmail.com>
2022-07-01 16:10:08 +0530

    questions practice 3

--- JS/concepts/freecodecampQues.js
+++ JS/concepts/freecodecampQues.js
@@ -439,4 +439,213 @@ myList.push(["Noodles", 30]);
 
 
 
+// Ques 49 :- Write Reusable JavaScript with Functions
+
+function reusableFunction(){
+    console.log('Hi World');
+  }
+  
+  reusableFunction();
+
+
+  // Ques 50  :-  Passing Values to Functions with Arguments
+
+function functionWithArgs(num1, num2){
+    console.log(num1+num2);
+  }
+  
+  functionWithArgs(3,7);
+
+
+
+  // Ques 51:- Return a Value from a Function with Return
+
+function timesFive(num){
+    return num*5;
+}
+
+
+
+// Ques 52:- Global Scope and Functions
+
+
+// Declare the myGlobal variable below this line
+let myGlobal = 10
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5
+
+}
+
+// Only change code above this line
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+
+
+
+
+// Ques 53:- Local Scope and Functions
+
+function myLocalScope() {
+    // Only change code below this line
+    let myVar = 10
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+  
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log('outside myLocalScope', myVar);
+
+
+
+  // Ques 54 :- Global vs. Local Scope in Functions
+
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  const outerWear = "sweater"
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+
+
+
+// // Setup
+// Ques 55:- Understanding Undefined Value returned from a Function
+
+
+
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive(){
+  sum = sum+5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
+
+
+
+
+//Ques 56:- Assignment with a Returned Value
+
+
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+
+processed = processArg(7);
+
+
+
+
+
+// a
+
+
+function nextInLine(arr, item) {
+    // Only change code below this line
+    arr.push(item);
+    item = arr.shift()
+    
+    return item;
+    // Only change code above this line
+  }
+  
+  // Setup
+  const testArr = [1, 2, 3, 4, 5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
+
+
+
+
+  // Ques 58:- Understanding Boolean Values
+
+
+function welcomeToBooleans() {
+    // Only change code below this line
+  
+    return true; // Change this line
+  
+    // Only change code above this line
+  }
+  
+
+// Ques 59 :- Use Conditional Logic with If Statements
+
+function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+    if(wasThatTrue){
+      return "Yes, that was true";
+    }
+    return "No, that was false";
+    // Only change code above this line
+  }
+
+
+
+
+  // Ques 60:- Comparison with the Equality Operator
+
+
+
+// Setup
+function testEqual(val) {
+    if (val==12) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testEqual(10);
+
+
+
+  
+// Ques 61:- Comparison with the Strict Equality Operator
+// Setup
+function testStrict(val) {
+    if (val===7) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testStrict(10);
+  
+
+
+
+
 
