5b53e52617f16982d634ba4332a0f2f4dfa87a3f
SujeetM19 <sujeetmishra008@gmail.com>
2022-07-09 00:20:01 +0530

    hoisting basics

---
+++ JS/concepts/hoisting.js
@@ -0,0 +1,32 @@
+// Hoisting definition :- Javascript hoisting refers to the process whereby the interpretter appears to move the declaration of functions, variables, or classes to th top  of their scope, prior to the execution of code.
+// kehne ka matlab hai ki we can imagine ki jo variable hai hamara humne ek tarike se usko phle hi upar declare kr diya hai hoisting m 
+
+console.log(x);
+greet();
+var x = 10;
+function greet(){
+    console.log("Hello");
+}
+
+
+//Use memory diagram to solve the problem of hoisting
+//memory diagram agar aata hai to sab sahi hoga!!
+
+
+//hoisting ki definition  jaisa koi code exist nahi krt, agar memory diagram aata hai to pata rahega ki aisa hi hona tha already. but hoisting ke according variables ek tarike se phle declare kr diye hai humne which is kind of not satisfactory.
+
+//
+
+
+
+
+
+
+// hoisting ki wajah se hinderence nahi banta, code chalta rehta hai. Hosting is a phenomenon jiski wajah se code hamare rukta nahi hai + hoisting mei ek tarike se upar declare ho jate hai variable declare ho jaate hai +  memory diagram ke hisaab se hi hota h sab
+
+
+ 
+
+
+
+
