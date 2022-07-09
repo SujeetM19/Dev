99268a4338448198629d9077a6189428a1f5c702
SujeetM19 <sujeetmishra008@gmail.com>
2022-07-09 10:40:34 +0530

    lexical scope

--- JS/concepts/hoisting.js
+++ JS/concepts/hoisting.js
@@ -1,12 +1,12 @@
 // Hoisting definition :- Javascript hoisting refers to the process whereby the interpretter appears to move the declaration of functions, variables, or classes to th top  of their scope, prior to the execution of code.
 // kehne ka matlab hai ki we can imagine ki jo variable hai hamara humne ek tarike se usko phle hi upar declare kr diya hai hoisting m 
 
-console.log(x);
-greet();
-var x = 10;
-function greet(){
-    console.log("Hello");
-}
+///console.log(x);
+///greet();
+//var x = 10;
+//function greet(){
+   // console.log("Hello");
+//}
 
 
 //Use memory diagram to solve the problem of hoisting
@@ -24,6 +24,36 @@ function greet(){
 
 // hoisting ki wajah se hinderence nahi banta, code chalta rehta hai. Hosting is a phenomenon jiski wajah se code hamare rukta nahi hai + hoisting mei ek tarike se upar declare ho jate hai variable declare ho jaate hai +  memory diagram ke hisaab se hi hota h sab
 
+
+// Ques :- 
+
+console.log(a); //undefined
+greet();        // Hello guys
+//rock();         //undefined (error aa raha hai)
+var a = 10;
+
+console.log(rock);
+
+
+
+function greet(){
+    console.log("Hello guys");
+}
+
+//function expression mei thoda alag hota  hai
+
+
+var rock = function(){
+    console.log("You guys rock");
+}
+
+
+//agar iske baar hello(); likhenge to run karega!
+
+//memory creation ke time jagah assign nahi hoti hai. sirf memory allloacte hoti hai, variables assign nahi hote hai!
+
+
+//function  expression ke case mei bhiundefined ko kaat kar poora function definition aa jata hai memory creating space mei.
 
  
 
---
+++ JS/concepts/scope.js
@@ -0,0 +1,17 @@
+function outer(){
+    inner();
+    function inner(){
+        console.log(b);
+    }
+}
+
+var b = 10;
+outer();
+console.log(b);
+
+
+/// 10 10 using memory diagram
+
+//Lexical scope , Lexical environment chaining
+//jo local execution contexts hote hai wo apnne saath apne parents ki bhi lexical environment lekr chalte hai
+
